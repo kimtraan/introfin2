1a9169050d9a25d11423c8e09c08fa4971765c27
kimtraan <kim.ttraan@gmail.com>
2022-02-21 14:56:56 -0800

    linegraph

--- src/components/Linegraph.js
+++ src/components/Linegraph.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Line} from 'react-chartjs-2';
 
 function Linegraph() {
+    const [ graphData, setGraphData ] = useState ([]);
     const data = 
     [{
         x:10,
@@ -9,8 +10,28 @@ function Linegraph() {
     }, {
         x:15,
         y:10
-    }]
+    }, {
+        x: 12,
+        y: 4,
+    }
+]
+  
+  const createMockData = () => {
+      let data = [];
+      let value = 50;
+      for(var i = 0; i< 366; i++) {
+          let date = new Date();
+          date.setHours(0,0,0,0);
+          date.setDate(i);
+          value += Math.round((Math.random() < 0.5 ? 1 : 0) * Math.random() *10);
+          data.push({x:date, y:value});
+      }
+      setGraphData(data);
+  }
+  useEffect (()=>{
+      createMockData();
 
+  }, [])
   return (
     <div className = 'linegraph'>
         <Line
@@ -18,9 +39,43 @@ function Linegraph() {
                 datasets: [
                     {
                         type: "line",
-                        data: data
+                        data: graphData,
+                        backgroundColor: "black",
+                        borderColor: '#5AC53B',
+                        borderWidth: 2,
+                        pointBorderColor: 'rgba(0,0,0,0)',
+                        pointBackgroundColor: '#5AC53B',
+                        pointHoverBackgroundColor: '#000000',
+                        pointHoverBorderWidth: 4,
+                        pointHoverRadius: 6,
                     }
                 ]
+            }}
+            options = {{
+                tooltips: {
+                    mode: "index",
+                    intersect: false
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            type: "time",
+                            time: {
+                                format: "MM/DD/YY",
+                                tooltipFormat: "ll",
+                            },
+                            ticks: {
+                                display: false,
+                            }
+                        },
+                    ],
+                    yAxes: [{
+                        ticks: {
+                            display: false
+                        }
+
+                    }]
+                }
             }}
         />
     </div>
